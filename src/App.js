cf33b4763731b349e3f7227c788d8ff525121908
ronan-f <rfegan13@gmail.com>
2019-09-16 21:08:55 -0700

    Added /shop route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
+import ShopPage from './Pages/Shop/shop.component';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
