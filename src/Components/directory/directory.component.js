ab0dfa7547d1fae570dc53f4b6d14f087071512c
ronan-f <rfegan13@gmail.com>
2019-09-16 19:25:56 -0700

    Added background images to menu items

--- src/Components/directory/directory.component.js
+++ src/Components/directory/directory.component.js
@@ -5,43 +5,43 @@ import './directory.styles.scss';
 const sections = [
   {
     title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    imageUrl: 'https://images.unsplash.com/photo-1529958030586-3aae4ca485ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
     id: 1,
     linkUrl: 'shop/hats'
   },
   {
     title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    imageUrl: 'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
     id: 2,
     linkUrl: 'shop/jackets'
   },
   {
     title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    imageUrl: 'https://images.unsplash.com/photo-1550521011-1d9d270e6d90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
     id: 3,
     linkUrl: 'shop/sneakers'
   },
   {
     title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    imageUrl: 'https://images.unsplash.com/photo-1504194921103-f8b80cadd5e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
     size: 'large',
     id: 4,
-    linkUrl: 'shop/womens'
+    linkUrl: 'shop/womens',
   },
   {
     title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    imageUrl: 'https://images.unsplash.com/photo-1472591651607-70e2d88ae3c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1556&q=80',
     size: 'large',
     id: 5,
-    linkUrl: 'shop/mens'
+    linkUrl: 'shop/mens',
   }
 ];
 
 
 const Directory = () => (
   <div className="directory-menu">
-    {sections.map(({ title, imageUrl, id }) => (
-      <MenuItem key={ id } title={title} subtitle={"SHOP NOW"} imageUrl={imageUrl} />
+    {sections.map(({ title, imageUrl, id, size }) => (
+      <MenuItem key={id} title={title} subtitle={"SHOP NOW"} imageUrl={imageUrl} size={size} />
     ))}
   </div>
 )
--- src/Components/menu-item/menu-item.component.js
+++ src/Components/menu-item/menu-item.component.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, subtitle }) => (
-  <div className="menu-item">
+const MenuItem = ({ title, subtitle, imageUrl, size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    className={`${size} menu-item`}
+  >
     <div className="content">
       <h1 className="title">{title}</h1>
       <span>{subtitle}</span>
