1a45592a6dea1dd872e67e28fe34ab0947d79e78
ronan-f <rfegan13@gmail.com>
2019-09-17 21:32:18 -0700

    Added submit button

--- src/Components/sign-in/SignIn.component.js
+++ src/Components/sign-in/SignIn.component.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const SignIn = () => {
-  const [form, updateForm] = React.useState({
+  const initialState = {
     email: "",
     password: ""
-  });
+  };
+
+  const [form, updateForm] = React.useState(initialState);
 
   const _handleChange = (e) => {
     updateForm({...form, [e.target.name]: e.target.value})
+  }
+
+  const _handleSubmit = (e) => {
+    e.preventDefault();
+    updateForm(initialState);
   }
 
   return (
@@ -15,11 +22,13 @@ const SignIn = () => {
       <h2>I have an account</h2>
       <span>Sign in with email and password</span>
 
-      <form>
+      <form onSubmit={_handleSubmit}>
         <input name="email" onChange={_handleChange} value={form.email} required />
         <label>Email</label>
         <input name="password" onChange={_handleChange} value={form.password} required />
         <label>Password</label>
+
+        <button type="submit" value="Submit Form">Log In</button>
       </form>
     </div>
   )
