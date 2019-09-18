591b0c60652907a78efa8970895ea6367f786a78
ronan-f <rfegan13@gmail.com>
2019-09-17 21:02:10 -0700

    Created header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/Shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
+import Header from './Components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
