1c9d8da3b74cf1c54905cc966ba73dc99756d0db
ronan-f <rfegan13@gmail.com>
2019-09-17 20:43:32 -0700

    Mapping over each collection item to render

--- src/Components/preview-collection/preview-collection.component.js
+++ src/Components/preview-collection/preview-collection.component.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const PreviewCollection = ({title, items}) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-      {items.name}
+      {
+        items.filter((item, index) => index < 4).map(({id, ...otherProps}) => (
+        <CollectionItem key={id} {...otherProps} />
+      ))
+      }
     </div>
   </div>
 )
