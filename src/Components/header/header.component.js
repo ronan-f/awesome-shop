6c8a37a02199e9ac586f807e73b3a65aaf85c2c3
ronan-f <rfegan13@gmail.com>
2019-09-20 21:30:38 -0700

    Added signout to header

--- src/Components/header/header.component.js
+++ src/Components/header/header.component.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/crown.svg';
 import './header.styles.scss';
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
-      <Logo className="logo"/>
+      <Logo className="logo" />
     </Link>
     <div className="options">
       <Link className="option" to="/shop">
@@ -15,9 +16,16 @@ const Header = () => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
-      <Link className="option" to="/signin">
-        SIGN IN
-      </Link>
+      {
+        currentUser ?
+          <div className="option" onClick={() => auth.signOut()}> SIGN OUT </div>
+          :
+          <Link className="option" to="/signin">
+            SIGN IN
+        </Link>
+
+      }
+
     </div>
   </div>
 )
