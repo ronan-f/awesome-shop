d2276b8a60c6668acaf8e973739692ec350349b1
ronan-f <rfegan13@gmail.com>
2019-09-22 17:02:31 -0700

    Added sign up functionality

--- src/Components/sign-up/SignUp.component.js
+++ src/Components/sign-up/SignUp.component.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+import './SignUp.styles.scss';
 
 const initialState = {
-  name: "",
+  displayName: "",
   email: "",
   password: ""
 }
@@ -18,16 +20,30 @@ const SignUp = () => {
     })
   }
 
-  const { name, email, password } = state;
+  const _handleSubmit = async (e) => {
+    try {
+
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      await createUserProfileDocument(user, displayName)
+      setState(initialState);
+
+    } catch(e) {
+      console.error(e.message);
+    }
+  }
+
+  const { displayName, email, password } = state;
 
   return (
-    <div>
+    <div className="sign-up">
       <h2>I don't have an account</h2>
       <span>Sign up with your email/password</span>
-      <FormInput name="name" label="Name" _handleChange={_handleChange} value={name} requried />
-      <FormInput name="email" label="Email" _handleChange={_handleChange} value={email} required />
-      <FormInput name="password" label="password" _handleChange={_handleChange} value={password} required />
-      <CustomButton type="submit">Sign Up</CustomButton>
+      <form onSubmit={_handleSubmit}>
+        <FormInput name="name" label="Name" _handleChange={_handleChange} value={displayName} requried />
+        <FormInput name="email" label="Email" _handleChange={_handleChange} value={email} required />
+        <FormInput name="password" label="Password" _handleChange={_handleChange} value={password} required />
+        <CustomButton type="submit">Sign Up</CustomButton>
+      </form>
     </div>
   );
 }
