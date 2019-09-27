e5a5c9e7c9cffbb828a93dca7e052a130d3c7933
ronan-f <rfegan13@gmail.com>
2019-09-26 20:42:38 -0700

    If signed in redirect to /

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/Shop/shop.component';
 import SignInUp from './Pages/SignInUp/SignInUp.component';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Header from './Components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -45,15 +45,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInUp} />
+          <Route exact path="/signin" render={ () => this.props.currentUser ? <Redirect to="/" /> : <SignInUp /> } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
