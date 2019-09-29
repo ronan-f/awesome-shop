37c75075dc6c2591d3fe5130ad4e7018bb63deac
ronan-f <rfegan13@gmail.com>
2019-09-28 20:57:17 -0700

    Memorize cart icon selector

--- src/Components/cart-icon/cart-icon.component.js
+++ src/Components/cart-icon/cart-icon.component.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../assets/shopping-bag.svg";
 import { toggleCart } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
 import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCart, itemCount }) => (
@@ -16,8 +17,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCart: () => dispatch(toggleCart())
 });
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
