d3a34a36e4045180d8a0cf36ee24c22dfc78a30e
ronan-f <rfegan13@gmail.com>
2019-09-29 15:15:20 -0700

    Added functionality to remove items from cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -20,6 +20,12 @@ const cartDropdownReducer = (state = INITIAL_STATE, action) => {
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
 
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
+      }
+
     default:
       return state;
   }
