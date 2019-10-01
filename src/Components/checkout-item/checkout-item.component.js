df7d151d59865f7efe0945353cf2a790c8bc2cc7
ronan-f <rfegan13@gmail.com>
2019-09-30 20:45:32 -0700

    Implemented functionality to add/remove items from cart using arrows

--- src/Components/checkout-item/checkout-item.component.js
+++ src/Components/checkout-item/checkout-item.component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './checkout-item.styles.scss';
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
 
   return (
@@ -13,9 +13,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
       </div>
       <span className='name'>{ name }</span>
       <span className='quantity'>
-        <div className='arrow'>&#10094;</div>
+        <div onClick={() => removeItem(cartItem)} className='arrow'>&#10094;</div>
         <span className='value'>{ quantity }</span>
-        <div className='arrow'>&#10095;</div>
+        <div onClick={() => addItem(cartItem)} className='arrow'>&#10095;</div>
       </span>
       <span className='price'>{ price }</span>
       <div onClick={ () => clearItem(cartItem) } className='remove-button'>&#10005;</div>
@@ -24,7 +24,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 };
 
 const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItemFromCart(item))
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
