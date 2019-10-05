608da23f63d4cc033ddf6c93b4941789f468e11c
ronan-f <rfegan13@gmail.com>
2019-10-05 10:42:15 -0700

    Render specific collections within collection component

--- src/Pages/Collection/Collection.component.js
+++ src/Pages/Collection/Collection.component.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop.selectors';
-import PreviewCollection from '../../Components/preview-collection/preview-collection.component';
 import './Collection.styles.scss';
+import CollectionItem from '../../Components/collection-item/collection-item.component';
 
 const CollectionPage = ({ collection }) => {
+  const { title, items } = collection
     return (
-    <div className='category'>
-      <PreviewCollection title={"Hats"} items={ collection } />
+    <div className='collection-page'>
+      <h2 className='title'>{ title }</h2>
+      <div className='items'>
+      {
+        items.map(item => <CollectionItem key={ item.id } item={ item } />)
+      }
+      </div>
     </div>
   )
   }
