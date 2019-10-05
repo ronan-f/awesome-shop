261a44a0ca863774acc1c9140cd96e7506d21ff1
ronan-f <rfegan13@gmail.com>
2019-10-05 11:06:43 -0700

    Added a selector which generates an array of collections

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,6 +5,11 @@ const selectShop = state => state.shop;
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
 )
 
 export const selectCollection = collectionId => createSelector(
