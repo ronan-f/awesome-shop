7790688585889a2649df175fbaf189d55475a5ab
ronan-f <rfegan13@gmail.com>
2019-10-06 21:12:32 -0700

    Added function to load firebase collection

--- src/Pages/Shop/shop.component.js
+++ src/Pages/Shop/shop.component.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../Components/collections-overview/collections-overview.component';
 import CollectionPage from '../Collection/Collection.component';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-const ShopPage = ({ match }) => (
-  <div>
-    <Route exact path={ match.path } component={ CollectionsOverview } />
-    <Route path={ `${match.path}/:collectionId` } component={ CollectionPage} />
-  </div>
-);
+const ShopPage = ({ match }) => {
+
+  // let unsubscribeFromSnapshot = null;
+
+  useEffect(() => {
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async snapshot => {
+      convertCollectionsSnapshotToMap(snapshot);
+    })
+  });
+
+  return (
+    <div>
+      <Route exact path={ match.path } component={ CollectionsOverview } />
+      <Route path={ `${match.path}/:collectionId` } component={ CollectionPage} />
+    </div>
+  );
+};
 
 export default (ShopPage);
