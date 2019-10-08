ccfd597cde24929844d4cc9b8d75a32bb2d07560
ronan-f <rfegan13@gmail.com>
2019-10-07 19:53:38 -0700

    Added redux function to get shop items from firestore

--- src/Pages/Shop/shop.component.js
+++ src/Pages/Shop/shop.component.js
@@ -3,17 +3,20 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../Components/collections-overview/collections-overview.component';
 import CollectionPage from '../Collection/Collection.component';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
+import { connect } from 'react-redux';
 
-const ShopPage = ({ match }) => {
+const ShopPage = ({ match, updateCollections }) => {
 
   // let unsubscribeFromSnapshot = null;
 
   useEffect(() => {
     const collectionRef = firestore.collection('collections');
     collectionRef.onSnapshot(async snapshot => {
-      convertCollectionsSnapshotToMap(snapshot);
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
     })
-  });
+  }, [updateCollections]);
 
   return (
     <div>
@@ -23,4 +26,8 @@ const ShopPage = ({ match }) => {
   );
 };
 
-export default (ShopPage);
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage);
